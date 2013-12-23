e0255b48511efd7ee5adb92f67cb866a05a0829d
srossross <srossross@gmail.com>
2013-12-22 16:29:55 -0800

    updated docs

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 COMMIT_HASH=`git show --quiet | head -n1`
 VERSION=`python -c "print '$COMMIT_HASH'.split()[-1][:8]"`
-BUCKET="docs.wakari.io"
+BUCKET="docs.binstar.org"
 
 # git tag "$VERSION"
+hyde gen
 
 s3cmd -c .s3cfg sync --recursive deploy/ s3://"$BUCKET"/"$VERSION"/
 s3cmd -c .s3cfg sync --recursive deploy/ s3://"$BUCKET"/
