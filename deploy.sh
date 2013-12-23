1a982a5a806c94df9dd1aa6559db3358e9227ea4
srossross <srossross@gmail.com>
2013-12-22 23:17:17 -0800

    updated docs

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ BUCKET="docs.binstar.org"
 # git tag "$VERSION"
 hyde gen
 
-s3cmd -c .s3cfg sync --recursive deploy/ s3://"$BUCKET"/"$VERSION"/
 s3cmd -c .s3cfg sync --recursive deploy/ s3://"$BUCKET"/
 
 # echo "Don't Forget to update the binstar_app Config file! VERSION=$VERSION"
