95754fb2ff44d51246d4a458cf2ee2c795559a3b
srossross <srossross@gmail.com>
2014-07-16 13:57:39 -0400

    ENH: udpated deploy and deploydraft scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ BUCKET="docs.binstar.org"
 # git tag "$VERSION"
 hyde gen
 
-s3cmd -c .s3cfg sync --recursive deploy/ s3://"$BUCKET"/
+s3cmd -c ~/.s3cfg sync --recursive deploy/ s3://"$BUCKET"/
 
 # echo "Don't Forget to update the binstar_app Config file! VERSION=$VERSION"
 echo 
--- scripts/deploydraft.sh
+++ scripts/deploydraft.sh
@@ -5,7 +5,7 @@ BUCKET="docs.binstar.org"
 # git tag "$VERSION"
 hyde gen
 
-s3cmd -c .s3cfg sync --recursive deploy/ s3://"$BUCKET"/draft/
+s3cmd -c ~/.s3cfg sync --recursive deploy/ s3://"$BUCKET"/draft/
 
 # echo "Don't Forget to update the binstar_app Config file! VERSION=$VERSION"
 echo 
