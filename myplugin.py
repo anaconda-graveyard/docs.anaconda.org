c4882775801984c72ce8d7a24dde575c61ccab97
srossross <srossross@gmail.com>
2014-08-07 12:30:08 -0700

    updating layout

---
+++ myplugin.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Contains classes and utilities related to meta data in hyde.
+"""
+
+from collections import namedtuple
+from functools import partial
+from itertools import ifilter
+from operator import attrgetter
+import re
+import sys
+
+from hyde.exceptions import HydeException
+from hyde.model import Expando
+from hyde.plugin import Plugin
+from hyde.site import Node, Resource
+from hyde.util import add_method, add_property, pairwalk
+
+import traceback
+from fswrap import File, Folder
+import yaml
+
+
+def is_active(resource, menue_item):
+    try:
+
+        url = menue_item.get('url', None)
+        if url is None:
+            return False
+        if url.endswith('/'):
+            url += 'index.html'
+        if resource.source.path.endswith(url):
+            print 'menue_item', menue_item
+            return 'active'
+    except:
+            traceback.print_exc()
+            raise
+
+class MyPlugin(Plugin):
+    def __init__(self, site):
+        self.site = site.is_active = is_active
