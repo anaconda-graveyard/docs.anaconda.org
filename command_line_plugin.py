f977b40df7b47d5345a998ba7db92d3ffe141ac8
srossross <srossross@gmail.com>
2014-11-05 07:37:32 -0800

    ENH: updated docs

--- command_line_plugin.py
+++ command_line_plugin.py
@@ -14,6 +14,9 @@ def make_anchor(s):
     anchor = re.sub('\W', ' ', s).title().replace(' ', '')
     return anchor
 
+def link(anchor):
+    return '<a id="%s"  href="#%s" name="%s" class="section-anchor">&nbsp;</a>' % (anchor, anchor, anchor)
+
 class CommandLinePlugin(Plugin):
     def __init__(self, site):
 
@@ -25,7 +28,9 @@ class CommandLinePlugin(Plugin):
         self.index = []
 
         self.globals = {'command_line_help': self.command_line_help,
-                        'page_header': self.page_header}
+                        'json_command_line_help': self.json_command_line_help,
+                        'page_header': self.page_header,
+                        'edit': self.edit}
 
     SECTION_TAG = '<div class="method-section"><div class="method-description">{%filter markdown%}'
     EXAMPLE_TAG = '{%endfilter%}</div><div class="method-example">{%filter markdown%}'
@@ -47,6 +52,18 @@ class CommandLinePlugin(Plugin):
             text = re.sub(pattern, repl, text)
         return Plugin.text_resource_complete(self, resource, text)
 
+    def edit(self, template):
+        filename = template._TemplateReference__context.name
+        repo = self.site.config.context.data.repo
+        branch = self.site.config.context.data.get('branch', 'master')
+
+        url = '%s/edit/%s/content/%s' % (repo, branch, filename)
+
+        result = '<a title="Edit on Github" href="%s">' % url
+        result += '<span class="glyphicon glyphicon-pencil"></span></a>'
+
+        return result
+
     def page_header(self, template, resource, title, h=2):
         result = resource.add_index(title)
         filename = template._TemplateReference__context.name
@@ -56,8 +73,7 @@ class CommandLinePlugin(Plugin):
 
         url = '%s/edit/%s/content/%s' % (repo, branch, filename)
         result += '<h%i>%s' % (h, title)
-        result += '<small class="pull-right"><a title="Edit on Github" href="%s">' % url
-        result += '<span class="glyphicon glyphicon-pencil"><span></a>'
+        result += '<small class="pull-right">%s' % self.edit(template)
         result += '</small></h%i>' % h
         return result
 
@@ -73,34 +89,31 @@ class CommandLinePlugin(Plugin):
 
         resource.xindex = []
 
-        def r_add_index(title):
+        def r_add_index(title, level=1):
             anchor = make_anchor(title)
-            resource.xindex.append([title, anchor, []])
-            return '<a id="%s"  href="#%s" name="%s" class="section-anchor">&nbsp;</a>' % (anchor, anchor, title)
+            xindex = resource.xindex
+            while level > 1:
+                xindex = xindex[-1][-1]
+                level -= 1
+            xindex.append([title, anchor, []])
+            return link(anchor)
+
         resource.add_index = r_add_index
-
-        def r_add_subindex(title):
-            if resource.xindex:
-                sub_index = resource.xindex[-1][-1]
-                anchor = make_anchor(title)
-                sub_index.append([title, anchor, []])
-                return '<a id="%s"  href="#%s" name="%s" class="section-anchor">&nbsp;</a>' % (anchor, anchor, title)
-
-        resource.add_subindex = r_add_subindex
-
-        def r_add_subsubindex(title):
-            if resource.xindex:
-                sub_index = resource.xindex[-1][-1]
-                subsub_index = sub_index[-1][-1]
-                anchor = make_anchor(title)
-                subsub_index.append([title, anchor, []])
-                return '<a id="%s" href="#%s" name="%s" class="section-anchor">&nbsp;</a>' % (anchor, anchor, title)
-
-        resource.add_subsubindex = r_add_subsubindex
+        resource.add_subindex = lambda title: r_add_index(title, 2)
+        resource.add_subsubindex = lambda title: r_add_index(title, 3)
 
         self.template.env.globals.update(self.globals)
 
         return Plugin.begin_text_resource(self, resource, text)
+
+    def json_command_line_help(self, shell):
+        try:
+            import json
+            output = check_output(shell, shell=True, stderr=STDOUT)
+            return json.loads(output)
+        except CalledProcessError as err:
+            output = err.output
+        return output
 
     def command_line_help(self, shell):
         if not os.path.isdir('cache'):
