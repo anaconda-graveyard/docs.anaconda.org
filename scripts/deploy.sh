c12f140f8a9af95a7b7cf0fae8832046a901e68b
Will Warner <electronwill@gmail.com>
2016-01-06 19:33:57 -0600

    Revert "enable --delete-removed on deploy.sh and deploydraft.sh"

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ BUCKET="docs.anaconda.org"
 # git tag "$VERSION"
 hyde gen -r -c prod.yaml
 
-s3cmd -c ~/.s3cfg sync --recursive --delete-removed deploy/ s3://"$BUCKET"/
+s3cmd -c ~/.s3cfg sync --recursive deploy/ s3://"$BUCKET"/
 
 # echo "Don't Forget to update the binstar_app Config file! VERSION=$VERSION"
 echo
